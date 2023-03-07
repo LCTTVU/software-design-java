175da4839c37f8445a113432301c473df8999e1d
LCTTVU <lecungtuantranh@student.vu.nl>
2023-03-07 19:59:08 +0100

    basic home screen

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -7,6 +7,7 @@ import java.awt.event.*;
 public class Main {
 
     public static Frame currFrame;
+    public static String appFont = "Arial";
 
     public static void main (String[] args) {
         System.out.println("Welcome to Software Design");
@@ -17,20 +18,35 @@ public class Main {
 
     static Frame createHomeScreen() {
 
-        JButton button = new Button(Color.green,"Create Recipe", new Dimension(150,75));
-        button.addActionListener(new AbstractAction() {
+        JLabel title = new JLabel("HOME");
+        title.setFont(new Font(appFont,Font.BOLD,40));
+
+        JPanel titlePanel = new Panel(Color.red, new Dimension(100,100));
+        titlePanel.setLayout(new GridBagLayout());
+        titlePanel.add(title);
+
+        JPanel recipePanel = new Panel(Color.blue, new Dimension(400,100));
+
+
+
+        JPanel crPanel = new Panel(Color.gray, new Dimension(100,100));
+        crPanel.setLayout(new GridBagLayout());
+
+        JButton crButton = new Button(Color.green,"Create Recipe", new Dimension(250,75));
+        crButton.setFont(new Font(appFont,Font.PLAIN,25));
+        crButton.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 currFrame = createCrScreen();
             }
         });
-
-        JPanel crPanel = new Panel(Color.gray,new Dimension(100,100));
-        crPanel.setLayout(new GridBagLayout());
-        crPanel.add(button);
+        crPanel.add(crButton);
 
         Frame homeScreen = new Frame("Home Screen");
         homeScreen.setLayout(new BorderLayout());
+
+        homeScreen.add(titlePanel,BorderLayout.NORTH);
+        homeScreen.add(recipePanel,BorderLayout.CENTER);
         homeScreen.add(crPanel,BorderLayout.SOUTH);
 
         return homeScreen;
@@ -38,11 +54,10 @@ public class Main {
 
     static Frame createCrScreen() {
 
-        JLabel label = new JLabel();
-        label.setText("CREATE RECIPE");
-        label.setFont(new Font("Arial",Font.BOLD,25));
+        JLabel label = new JLabel("CREATE RECIPE");
+        label.setFont(new Font(appFont,Font.BOLD,40));
 
-        JPanel titlePanel = new Panel(Color.red,new Dimension(100,75));
+        JPanel titlePanel = new Panel(Color.red, new Dimension(100,100));
         titlePanel.setLayout(new GridBagLayout());
         titlePanel.add(label);
 
