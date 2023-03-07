74c1bcf07913127693478420bd84a06a57acb4eb
LCTTVU <lecungtuantranh@student.vu.nl>
2023-03-07 19:01:11 +0100

    basic switch screen button

--- src/main/java/softwaredesign/Button.java
+++ src/main/java/softwaredesign/Button.java
@@ -2,22 +2,14 @@ package softwaredesign;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Button extends JButton implements ActionListener {
+public class Button extends JButton{
     Button(Color color, String text, Dimension dimension){
         this.setText(text);
         this.setBackground(color);
-        this.addActionListener(this);
         this.setFocusable(false);
         this.setPreferredSize(dimension);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==this) {
-            System.out.println("create recipe");
-        }
-    }
+
 }
--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,21 +1,56 @@
 package softwaredesign;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Main {
-    public static void main (String[] args){
+
+    public static Frame currFrame;
+
+    public static void main (String[] args) {
         System.out.println("Welcome to Software Design");
 
-        Frame newFrame = new Frame("Home Screen");
-        newFrame.setLayout(new BorderLayout());
+        currFrame = createHomeScreen();
+    }
+
+
+    static Frame createHomeScreen() {
+
+        JButton button = new Button(Color.green,"Create Recipe", new Dimension(150,75));
+        button.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                currFrame = createCrScreen();
+            }
+        });
 
         JPanel crPanel = new Panel(Color.gray,new Dimension(100,100));
+        crPanel.setLayout(new GridBagLayout());
+        crPanel.add(button);
 
-        JButton createRecipeButton = new Button(Color.green,"Create Recipe", new Dimension(150,75));
+        Frame homeScreen = new Frame("Home Screen");
+        homeScreen.setLayout(new BorderLayout());
+        homeScreen.add(crPanel,BorderLayout.SOUTH);
 
-        crPanel.add(createRecipeButton);
-        newFrame.add(crPanel,BorderLayout.SOUTH);
+        return homeScreen;
     }
+
+    static Frame createCrScreen() {
+
+        JLabel label = new JLabel();
+        label.setText("CREATE RECIPE");
+        label.setFont(new Font("Arial",Font.BOLD,25));
+
+        JPanel titlePanel = new Panel(Color.red,new Dimension(100,75));
+        titlePanel.setLayout(new GridBagLayout());
+        titlePanel.add(label);
+
+        Frame crScreen = new Frame("Create Recipe");
+        crScreen.setLayout(new BorderLayout());
+        crScreen.add(titlePanel,BorderLayout.NORTH);
+
+        return crScreen;
+    }
+
 }
