aabe7ad8d332a2e22c1ede16bb80506c78a9f0dc
LCTTVU <lecungtuantranh@student.vu.nl>
2023-03-16 19:55:13 +0100

    Update HomeController.java

--- src/main/java/softwaredesign/HomeController.java
+++ src/main/java/softwaredesign/HomeController.java
@@ -9,8 +9,6 @@ import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
